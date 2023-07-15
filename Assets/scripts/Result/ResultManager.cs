bb313f92037ddb86bb3c335aeff0d8589f7caa06
AlUEO <sdt22csc0005@edu.sca.ac.jp>
2023-07-15 12:11:53 +0900

    test

--- Assets/scripts/Result/ResultManager.cs
+++ Assets/scripts/Result/ResultManager.cs
@@ -9,10 +9,20 @@ public class ResultManager : MonoBehaviour
 {
     [SerializeField]
     private Text TotalScoretext;
+    [SerializeField]
+    private Text Bonustext;//3
+    [SerializeField]
+    private Text NotFakeScoretext;//2
+    [SerializeField]
+    private Text Questiontext;//1
+    [SerializeField]
+    private float WaitTime;
+    [SerializeField]
+    private float t;
     // Start is called before the first frame update
     void Start()
     {
-        TotalScoretext.text=ScoreCounter.TotalScore.ToString()+"–œ‰~";
+        StartCoroutine(Result());
     }
 
     // Update is called once per frame
@@ -23,5 +33,22 @@ public class ResultManager : MonoBehaviour
     public void OnClick()
     {
        SceneManager.LoadScene("TitleScene");
+    }
+
+    IEnumerator Result()
+    {
+        Questiontext.text=(GameManager.QuestionCount*1).ToString();
+        if(GameManager.NotFakeCount!=0)
+        {
+            yield return new WaitForSeconds(WaitTime);
+            NotFakeScoretext.text=(GameManager.NotFakeCount*8).ToString();
+        }
+        if(GameManager.BonusCount!=0)
+        {
+            yield return new WaitForSeconds(WaitTime);
+            Bonustext.text=(GameManager.BonusCount*15).ToString();
+        }
+        yield return new WaitForSeconds(WaitTime);
+        TotalScoretext.text = ScoreCounter.TotalScore.ToString();
     }
 }
